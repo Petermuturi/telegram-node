e6a3498548ef65f48e214ac5114d0af29d3ea5c2
Petermuturi <pmuturi65@gmail.com>
2016-11-03 16:55:39 +0300

    environment token

--- main/bot.js
+++ main/bot.js
@@ -1,14 +1,13 @@
 const TelegramBot = require('node-telegram-bot-api');
 const config = require('../config/token.js');
 
-const token = config.token;
-
+const token = process.env.TOKEN || config.token;
 
 module.exports={
 	runBot: (req, res)=> {
 		let bot = new TelegramBot(token, {polling: true});
 
-		bot.on('message', function (msg) {
+		bot.on('message', (msg)=> {
 		  let chatId = msg.chat.id;
 		  let photo = 'why-js.png';
 		  bot.sendPhoto(chatId, photo, {caption: 'I <3 JS'});
