4562dca65e8d362b6f94a9220ff443390d77bf97
Petermuturi <pmuturi65@gmail.com>
2016-11-03 17:03:55 +0300

    changed from es6

--- main/bot.js
+++ main/bot.js
@@ -1,15 +1,15 @@
-const TelegramBot = require('node-telegram-bot-api');
-const config = require('../config/token.js');
+var TelegramBot = require('node-telegram-bot-api');
+var config = require('../config/token.js');
 
-const token = process.env.TOKEN || config.token;
+var token = process.env.TOKEN || config.token;
 
 module.exports={
 	runBot: (req, res)=> {
-		let bot = new TelegramBot(token, {polling: true});
+		var bot = new TelegramBot(token, {polling: true});
 
 		bot.on('message', (msg)=> {
-		  let chatId = msg.chat.id;
-		  let photo = 'why-js.png';
+		  var chatId = msg.chat.id;
+		  var photo = 'why-js.png';
 		  bot.sendPhoto(chatId, photo, {caption: 'I <3 JS'});
 		});
 		res.send('running:)')
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-let app = express();
-const bot = require('./main/bot.js')
+var express = require('express');
+var app = express();
+var bot = require('./main/bot.js')
 
 app.get('/', bot.runBot);
 
-let port = Number(process.env.PORT || 3000);
+var port = Number(process.env.PORT || 3000);
 
 app.listen(port, function(){
 	console.log('server listening on port 3000');
